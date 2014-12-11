dba7790ba542d509025c963a05b3eda613f7457c
Andrew Morris <andrew.morris@unt.edu>
2014-12-10 23:08:29 -0600

    Finishing up project minus items

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,7 @@ class Item < ActiveRecord::Base
 	
 	has_attached_file :image, :styles => {:medium => "300x300>", :thumb => "100x100>"}
 	validates_attachment_content_type :image, :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
+
+	validates :image, presence: true
+	validates :description, presence: true
 end
